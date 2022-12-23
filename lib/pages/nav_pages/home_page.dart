d55748078fae45f885319b83352c7ee60b8dac79
Isaac Korankye Asante <xquisit31@gmail.com>
2022-12-23 13:55:09 +0000

    Working on the home page

--- lib/pages/nav_pages/home_page.dart
+++ lib/pages/nav_pages/home_page.dart
@@ -3,7 +3,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:travle_app/misc/colors.dart';
 import 'package:travle_app/widgets/app_large_text.dart';
+import 'package:travle_app/widgets/app_text.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -53,32 +55,97 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
           const SizedBox(height: 30),
           // ignore: avoid_unnecessary_containers
           Container(
-            child:  TabBar(
-              controller: _tabController,
-              labelColor: Colors.black,
-              unselectedLabelColor: Colors.grey,
-              tabs: const [
-              Tab(
-                text: "Places",
-              ),
-              Tab(
-                text: "Inspiration",
-              ),
-              Tab(
-                text: "emotions",
-              ),
-            ]),
-          ),
-          // ignore: avoid_unnecessary_containers
+              child: Align(
+            alignment: Alignment.centerLeft,
+            child: TabBar(
+                isScrollable: true,
+                labelPadding: const EdgeInsets.only(left: 20, right: 20),
+                controller: _tabController,
+                labelColor: Colors.black,
+                unselectedLabelColor: Colors.grey,
+                indicator:
+                    CircleTabIndicator(color: AppColors.mainColor, radius: 4),
+                tabs: const [
+                  Tab(
+                    text: "Places",
+                  ),
+                  Tab(
+                    text: "Inspiration",
+                  ),
+                  Tab(
+                    text: "emotions",
+                  ),
+                ]),
+          )),
+          // ignore: avoid_unnecessary_containers, sized_box_for_whitespace
           Container(
+            padding: const EdgeInsets.only(left: 20),
             height: 300,
             width: double.maxFinite,
-            child:  TabBarView(
-              controller: _tabController,
-              children: const [Text("data"), Text("data"), Text("data")]),
-          )
+            child: TabBarView(controller: _tabController, children: [
+              ListView.builder(
+                itemCount: 3,
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (BuildContext context, int index) {
+                  return Container(
+                    margin: const EdgeInsets.only(right: 15, top: 10),
+                    width: 200,
+                    height: 300,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      image: const DecorationImage(
+                          image: AssetImage('img/mountain.jpeg'),
+                          fit: BoxFit.cover),
+                    ),
+                  );
+                },
+              ),
+              const Text("data"),
+              const Text("data")
+            ]),
+          ),
+          const SizedBox(height: 30,),
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              AppLargeText(text: "Explore more", size: 22,),
+              AppText(text: "See all", color: AppColors.textColor1,)
+            ],
+          ),
+          ),
         ],
       ),
     );
+  }
+}
+
+class CircleTabIndicator extends Decoration {
+  final Color color;
+  double radius;
+  CircleTabIndicator({required this.color, required this.radius});
+
+  @override
+  BoxPainter createBoxPainter([VoidCallback? onChanged]) {
+    // TODO: implement createBoxPainter
+    return _CirclePainter(color: color, radius: radius);
+  }
+}
+
+class _CirclePainter extends BoxPainter {
+  final Color color;
+  double radius;
+  _CirclePainter({required this.color, required this.radius});
+  @override
+  void paint(Canvas canvas, Offset offset, ImageConfiguration configuration) {
+    // TODO: implement paint
+    Paint _paint = Paint();
+    _paint.color = color;
+    _paint.isAntiAlias = true;
+    final Offset CircleOffset = Offset(
+        configuration.size!.width / 2 - radius / 2,
+        configuration.size!.height - radius);
+    canvas.drawCircle(offset + CircleOffset, radius, _paint);
   }
 }
