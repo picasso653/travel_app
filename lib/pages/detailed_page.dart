499ac141f9b8b9bbf96614117e215e931663fbd3
Isaac Korankye Asante <xquisit31@gmail.com>
2023-01-11 21:51:37 +0000

    New page is going well

--- lib/pages/detailed_page.dart
+++ lib/pages/detailed_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:travle_app/widgets/app_large_text.dart';
 
 class DetailPage extends StatefulWidget {
   const DetailPage({super.key});
@@ -19,18 +20,56 @@ class _DetailPageState extends State<DetailPage> {
         child: Stack(
           children: [
             Positioned(
+              left: 0,
+              right: 0,
+              child: Container(
+              width: double.maxFinite,
+              height: 350,
+              decoration: const BoxDecoration(
+                image: DecorationImage(image: AssetImage('img/mountain.jpeg'),
+                fit: BoxFit.cover
+                ),
+                
+              ),
+            )),
+            Positioned(
               left: 20,
-              top: 70,
+              top: 50,
               child:
             Row(
               children: [
                 IconButton(onPressed: (){},
-                 icon: const Icon(Icons.menu),),
+                 icon: const Icon(Icons.menu), color: Colors.white,),
                  IconButton(onPressed: (){},
                   icon: const Icon(Icons.two_k))
               ],
-            )
-            )
+            ),
+            ),
+            Positioned(
+              top: 320,
+              child: Container(
+                padding: const EdgeInsets.only(left: 20, right: 20, top: 30),
+                width: MediaQuery.of(context).size.width,
+                height: 500,
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(30),
+                    topRight: Radius.circular(30)
+                  )
+                ),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        AppLargeText(text: "Yosemite"),
+                        AppLargeText(text: "\$250")
+                      ],
+                    )
+                  ],
+                ),
+              ))
           ],
         ),
       ),
