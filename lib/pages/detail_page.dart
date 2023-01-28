3f69c6f60d923073940d52d853996c5c06dea792
Isaac Korankye Asante <xquisit31@gmail.com>
2023-01-28 14:49:03 +0000

    code

--- lib/pages/detail_page.dart
+++ lib/pages/detail_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:travle_app/widgets/app_large_text.dart';
 
 class DetailPage extends StatefulWidget {
   const DetailPage({super.key});
@@ -13,7 +14,70 @@ class _DetailPageState extends State<DetailPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(),
+      body: Container(
+        width: double.maxFinite,
+        height: double.maxFinite,
+        child: Stack(
+          children: [
+            Positioned(
+                left: 0,
+                right: 0,
+                child: Container(
+                  width: double.maxFinite,
+                  height: 330,
+                  decoration: const BoxDecoration(
+                      image: DecorationImage(
+                          image: AssetImage("img/mountain.jpeg"),
+                          fit: BoxFit.cover)),
+                )),
+            Positioned(
+              left: 20,
+              top: 60,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  IconButton(
+                    onPressed: () {},
+                    icon: const Icon(Icons.menu),
+                    color: Colors.white,
+                  ),
+                  const SizedBox(
+                    width: 240,
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: const Icon(Icons.more_vert_rounded),
+                    color: Colors.white,
+                  )
+                ],
+              ),
+            ),
+            Positioned(
+                top: 290,
+                child: Container(
+                  padding: const EdgeInsets.only(left: 15,right: 15,top: 20),
+                  width: MediaQuery.of(context).size.width,
+                  height: 500,
+                  decoration: const BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(30),
+                          topRight: Radius.circular(30))),
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          AppLargeText(text: "Yosemite"),
+                          AppLargeText(text: "\$ 250")
+                        ],
+                      )
+                    ],
+                  ),
+                ))
+          ],
+        ),
+      ),
     );
   }
 }
