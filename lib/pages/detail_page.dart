e5f1d29c5bbc8460494b6f0e16da5d828a041c6e
Isaac Korankye Asante <xquisit31@gmail.com>
2023-01-29 19:02:10 +0000

    Detail page

--- lib/pages/detail_page.dart
+++ lib/pages/detail_page.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:travle_app/misc/colors.dart';
 import 'package:travle_app/widgets/app_large_text.dart';
+import 'package:travle_app/widgets/app_text.dart';
 
 class DetailPage extends StatefulWidget {
   const DetailPage({super.key});
@@ -11,6 +13,7 @@ class DetailPage extends StatefulWidget {
 }
 
 class _DetailPageState extends State<DetailPage> {
+  int gottenStars = 3;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -55,7 +58,7 @@ class _DetailPageState extends State<DetailPage> {
             Positioned(
                 top: 290,
                 child: Container(
-                  padding: const EdgeInsets.only(left: 15,right: 15,top: 20),
+                  padding: const EdgeInsets.only(left: 15, right: 15, top: 20),
                   width: MediaQuery.of(context).size.width,
                   height: 500,
                   decoration: const BoxDecoration(
@@ -64,12 +67,53 @@ class _DetailPageState extends State<DetailPage> {
                           topLeft: Radius.circular(30),
                           topRight: Radius.circular(30))),
                   child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       Row(
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
-                          AppLargeText(text: "Yosemite"),
-                          AppLargeText(text: "\$ 250")
+                          AppLargeText(
+                            text: "Yosemite",
+                            color: Colors.black.withOpacity(0.8),
+                          ),
+                          AppLargeText(
+                            text: "\$ 250",
+                            color: AppColors.mainColor,
+                          )
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Row(
+                        children: [
+                          const Icon(
+                            Icons.location_on,
+                            color: AppColors.mainColor,
+                          ),
+                          AppText(
+                            text: "USA Carlifornia",
+                            color: AppColors.textColor1,
+                          )
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Row(
+                        children: [
+                          Wrap(
+                            children: List.generate(5, (index) {
+                              return const Icon(
+                                Icons.star,
+                                color: AppColors.starColor,
+                              );
+                            }),
+                          ),
+                          AppText(
+                            text: '(4.0)',
+                            color: AppColors.textColor2,
+                          )
                         ],
                       )
                     ],
