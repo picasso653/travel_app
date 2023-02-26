b0cbda09a4d1b5642a375aba19e27272dd623be0
Isaac Korankye Asante <xquisit31@gmail.com>
2023-02-26 23:15:45 +0000

    daily code

--- lib/pages/detail_page.dart
+++ lib/pages/detail_page.dart
@@ -163,7 +163,7 @@ class _DetailPageState extends State<DetailPage> {
                       const SizedBox(height: 15,),
                       AppLargeText(text: 'Description', color: Colors.black.withOpacity(0.8), size: 21,),
                       const SizedBox(height: 10,),
-                      AppText(text: 'You must go for a travel. Travelling helps get rid of pressure. Go to the mountains to see nature.', color: AppColors.mainTextColor,)
+                      AppText(text: 'Travelling helps get rid of pressure. Go to the mountains to see nature.', color: AppColors.mainTextColor,)
                     ],
                   ),
                 )),
