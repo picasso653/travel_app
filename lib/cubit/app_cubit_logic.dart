ffbbdf07b10e689e74af47b29d25509268326e95
Isaac Korankye Asante <xquisit31@gmail.com>
2023-03-29 17:03:10 +0000

    Daily code: problem fixed using cubit

--- lib/cubit/app_cubit_logic.dart
+++ lib/cubit/app_cubit_logic.dart
@@ -3,6 +3,7 @@ import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:travle_app/cubit/app_cubit_states.dart';
+import 'package:travle_app/cubit/app_cubits.dart';
 import 'package:travle_app/pages/welcome_screen.dart';
 
 class AppCubitLogics extends StatefulWidget {
@@ -16,7 +17,7 @@ class _AppCubitLogicsState extends State<AppCubitLogics> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: BlocBuilder(builder: (context, state) {
+      body: BlocBuilder<AppCubits, CubitState>(builder: (context, state) {
         if (state is WelcomeState) {
           return const WelcomePage();
         } else {
